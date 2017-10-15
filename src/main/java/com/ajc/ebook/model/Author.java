2ff9ae113f231266c848de4edabfc837ed2ffb7c
Toumi b <btoumi17@hotmail.com>
2017-10-15 12:23:28 +0200

    add simple comment

--- src/main/java/com/ajc/ebook/model/Author.java
+++ src/main/java/com/ajc/ebook/model/Author.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-public class Author {
+public class Author { //make change
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
